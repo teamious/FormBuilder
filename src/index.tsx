94e0d1eab9d6e028e37eceac6b3a92ef9ca50df5
jogao <jogao1983@gmail.com>
2017-03-14 16:43:50 -0700

    clean package.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
-import { Router, Route, browserHistory } from 'react-router';
-
-let Routes = (
-    <Router history={browserHistory}>
-        <Route path='/' component={App}/>
-    </Router>
-);
 
 ReactDOM.render(
-    Routes,
+    <App />,
     document.getElementById('root')
 );

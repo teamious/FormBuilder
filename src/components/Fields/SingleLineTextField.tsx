45bc8f9ec196c629e1daf447f6ee3247c565be2d
niuyunsi <niuyunsi@gmail.com>
2017-03-16 15:03:42 -0700

    change variable and method name

--- src/components/Fields/SingleLineTextField.tsx
+++ src/components/Fields/SingleLineTextField.tsx
@@ -6,27 +6,29 @@ interface IState {
 
 interface IProps {
     field: IField;
-    value: string,
-    onChange: (newValue: string) => void;
+    textFieldValue: string,
+    textFieldOnChange: (newValue: string) => void;
 }
 
 export default class SingleLineTextField extends React.PureComponent<IProps, IState> {
     constructor(props: IProps) {
         super(props);
-        this.onChange = this.onChange.bind(this);
+        this.textFieldOnChange = this.textFieldOnChange.bind(this);
     }
 
     public render() {
         const { label, hint, required, unique } = this.props.field.options ? this.props.field.options : null;
         return (
             <div>
-                <label>{label}</label><br />
-                <input type='text' placeholder={hint} required={required} value={this.props.value} onChange={this.onChange}/>
+                <div>
+                    {label}
+                </div>
+                <input type='text' placeholder={hint} required={required} value={this.props.textFieldValue} onChange={this.textFieldOnChange}/>
             </div>
         );
     }
 
-    private onChange(event: any) {
-        this.props.onChange(event.target.value);
+    private textFieldOnChange(event: any) {
+        this.props.textFieldOnChange(event.target.value);
     }
 }

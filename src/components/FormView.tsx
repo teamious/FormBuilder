48a4a6bd295365a68630d90c34898bb9a7b15bc6
niuyunsi <niuyunsi@gmail.com>
2017-03-27 12:12:01 -0700

    init formView

---
+++ src/components/FormView.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as assign from 'object-assign';
+
+import * as data from '../data';
+
+interface IProps { }
+
+interface IState { }
+
+export default class FormView extends React.PureComponent<IProps, IState> {
+    render() {
+        return (
+            <div>FormView</div>
+        )
+    }
+}

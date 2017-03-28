63726700039aa85a42316db79efdd55ba86c11bf
Andrew <andrewsinner@gmail.com>
2017-03-28 13:25:43 -0700

    Add index file for exporting components

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,2 @@
-export * from './components/FieldOptionEditor';
-export * from './components/FieldSelector';
-export * from './components/FormBuilder';
-export * from './components/FormBuilderContext';
-export * from './components/FormSubmissionView';
-export * from './components/NestedFormBuilder';
-export * from './components/NestedFormSubmissionView';
-
-export * from './data/IDragDrop';
-export * from './data/IField';
-export * from './data/IFieldOptionEditor';
-export * from './data/IFieldBuilder';
-export * from './data/IFieldRender';
-export * from './data/FieldRegistry';
+export * from './components';
+export * from './data';

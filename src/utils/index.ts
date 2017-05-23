c4f490c46fe5b33cf417563cab1c56c6d0e407b5
jogao <jogao1983@gmail.com>
2017-05-23 11:02:25 -0700

    nested form entry should have ID instead of using index to track error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -51,3 +51,13 @@ export function getFieldSiblings(field: IField, fields: Array<IField>): Array<IF
 
     return null;
 }
+
+// Attribution: http://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+export function generateID(len: number = 5) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < len; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
